5329246fec8f8c1406deb6666d774dd7dcb69931
Zippy-Waluse <zippywaluse@gmail.com>
2024-07-13 17:22:58 +0300

    made changes

--- src/Podcast/index.js
+++ src/Podcast/index.js
@@ -1,33 +1,12 @@
 import React from 'react';
+import './module.css'
 import './index.css';
 
-
-const PlayButton = () => {
-  
-
-};
-
-
 const InteractiveBox = () => {
-    const playButtonStyle = {
-    };
-
-    const interactiveBoxStyle = {
-        display: 'inline-block',
-        backgroundColor: '#f0f0f0',
-        padding: '20px',
-        borderRadius: '10px',
-        width: '300px',
-        height: '200px',
-        position: 'relative',
-        cursor: 'pointer',
-        backgroundColor:'#964B00'
-    };
-
     return (
-        <div style={interactiveBoxStyle}>
-            <p className='box-text'>"videos coming soon stay tuned"</p>
-            <div style={playButtonStyle}></div>
+        <div className='interactiveBox'> 
+            <p className="box-text">"videos coming soon stay tuned"</p>
+            <div></div>
         </div>
     );
 };
@@ -35,18 +14,14 @@ const InteractiveBox = () => {
 const Podcast = () => {
     return (
         <div className='title'>
-            <h1 className='title'>Podcast</h1>
+            <h1 className="title">Podcast</h1>
             <br />
-            
-            <p className='dynamic-text'>Our videos coming soon</p>
+            <p className="dynamic-text">Our videos coming soon</p>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
                 {[...Array(3)].map((_, index) => (
                     <InteractiveBox key={index} />
                 ))}
-                <PlayButton />
             </div>
-
-            
         </div>
     );
 };
